b9f6781522f76737250419fc202b3dffe34ea02d
VaishaliJain0209 <30920358+VaishaliJain0209@users.noreply.github.com>
2017-12-21 15:12:50 +0530

    Fixed.

--- FSDAssignments-new/src/app/trade/components/Trades.js
+++ FSDAssignments-new/src/app/trade/components/Trades.js
@@ -24,7 +24,7 @@ class Trades extends Component{
     }
     
     componentWillReceiveProps(nextprops){
-        console.log(nextprops.trades);
+        console.log(nextprops);
     }
 
     componentDidMount() {
@@ -162,3 +162,23 @@ Trades.defaultProps = {
 Trades.propTypes = {
     
 }
+
+const mapStateToProps = (state) => {
+    return {
+        rightPanel: state.tradeState.rightPanel,
+        selected: state.tradeState.selected,
+        trades: state.tradeState.trades,
+        loading: state.tradeState.loading,
+        error: state.tradeState.error,
+        errorMessage: state.tradeState.errorMessage
+    }
+}
+
+ const mapDispatchToProps = (dispatch) => {
+    return {
+        actions: bindActionCreators(actions, dispatch),
+    }
+} 
+
+export default connect(mapStateToProps, 
+                    mapDispatchToProps) (Trades)
